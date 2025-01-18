48bc0c1f0f3fb247b6bafc6f8bd3182908ac3c9b
Louis Tsang <kawingt2@gmail.com>
2025-01-18 09:18:42 +0800

    expanded get word function to make it more flexible

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import gtts, csv, os
 
-def get_word_from_csv(filename):
+def get_word_from_csv(filename, target_column="word"):
     words = []
     with open(filename, mode='r') as f:
         csvFile = csv.DictReader(f)
         for row in csvFile:
-            words.append(row['word'])
+            words.append(row[target_column])
     return words
 
 def generate_audio(words):
@@ -16,5 +16,5 @@ def generate_audio(words):
             tts = gtts.gTTS(f'{word}', slow=True)
             tts.save(f'output/{word}.mp3')
 
-words = get_word_from_csv('words.csv')
+words = get_word_from_csv('phrase.csv', "phrase")
 generate_audio(words)
