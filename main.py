01d418029fd5851a113cd4eba787d07a330728bb
Louis Tsang <kawingt2@gmail.com>
2025-01-22 09:46:55 +0800

    parsed args and make user provide a file name to process

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import gtts, csv, os
+import gtts, csv, os, argparse
 from utils import find_duplicate
 
 def get_word_from_csv(filename: str, target_column: str ="word") -> list[str]:
@@ -17,7 +17,14 @@ def generate_audio(words: list[str]) -> None:
             tts = gtts.gTTS(f'{word}', slow=True)
             tts.save(f'output/{word}.mp3')
 
-words = get_word_from_csv('words.csv', "word")
+parser = argparse.ArgumentParser(prog="Lexi-tts", description="The text to speech tool for generating sound of the words. It's mainly used for the lexi project to create sound for a language learning game, but can also be used for other purposes", epilog="It's open source and in active development. So feel free to contribute")
+parser.add_argument('file_name', type=str, help='The program requires a file name to run. The file should be located in the same directory.')
+parser.add_argument('target_column', type=str, nargs="?", default="word", help="The program requires a target column to extract words from the csv. The default is set to 'word'")
 
+args = parser.parse_args()
+file_name = args.file_name
+target_column = args.target_column
+
+words = get_word_from_csv(file_name, target_column)
 generate_audio(words)
 find_duplicate(words)
